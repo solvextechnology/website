2ce23e7cdf273ef5010561e0148ce053da7dfd87
bitcoincloudmining <bitcoincloudminingindia@gmail.com>
2026-06-15 13:14:32 +0530

    Remove impact-site-verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,9 +62,6 @@ export const metadata: Metadata = {
     shortcut: "/favicon.svg",
     apple: "/favicon.svg",
   },
-  other: {
-    "impact-site-verification": "2992bd3a-6bfe-4801-a857-af94f165e731",
-  },
 };
 
 export default function RootLayout({
